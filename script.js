2fe6f4fd4caa091890318976290571c76914f712
shakil-1318 <shakiljust21@gmail.com>
2024-10-23 00:55:46 +0600

    add page

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
+
 const navbar = document.querySelector('.navbar');
+let lastScrollY = window.scrollY;
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY >= 56) {
-        navbar.classList.add('navbar-scrolled');
+    if (window.scrollY > lastScrollY && window.scrollY >= 56) {
+        // User is scrolling down, hide the navbar
+        navbar.classList.add('navbar-hidden');
     } else {
-        navbar.classList.remove('navbar-scrolled');
+        // User is scrolling up, show the navbar
+        navbar.classList.remove('navbar-hidden');
+        if (window.scrollY >= 56) {
+            navbar.classList.add('navbar-scrolled');
+        } else {
+            navbar.classList.remove('navbar-scrolled');
+        }
     }
+
+    // Update the last scroll position
+    lastScrollY = window.scrollY;
 });
+
 
 // AOS Js
 AOS.init();
+
+
+// Siwper JS
+
+let swiperCard = new Swiper(".card-content", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        600: { slidesPerView: 2 },
+        968: { slidesPerView: 3 },
+    },
+});
+
